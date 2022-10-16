c63472df1e55e6b3ddd66961429e8a59ba2d850a
Marcus Welander <mwthinker@yahoo.se>
2022-10-16 21:17:21 +0200

    Fix wrong problem number

--- 2021/04/src/main.cpp
+++ 2021/04/src/main.cpp
@@ -1,29 +1,30 @@
 #include <string>
 #include <sstream>
-#include <string_view>
 #include <vector>
-#include <iostream>
+
+
+#include <fmt/core.h>
 
 const std::vector Draw{7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1};
 
 const std::string Data = R"(
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
+	22 13 17 11  0
+	 8  2 23  4 24
+	21  9 14 16  7
+	 6 10  3 18  5
+	 1 12 20 15 19
 
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
+	 3 15  0  2 22
+	 9 18 13 17  5
+	19  8  7 25 23
+	20 11 10 24  4
+	14 21 16 12  6
 
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7
+	14 21 17 24  4
+	10 16 15  9 19
+	18  8 23 26 20
+	22 11 13  6  5
+	 2  0 12  3  7
 )";
 
 struct Value {
@@ -33,8 +34,20 @@ struct Value {
 
 class Board {
 public:
+	// Create empty board.
 	Board() {
-
+	}
+	
+	//	Create a board from numbers.
+	//
+	//	Assumes the following formats:
+	//		22 13 17 11  0
+	//		8   2 23  4 24
+	//		21  9 14 16  7
+	//		6  10  3 18  5
+	//		1  12 20 15 19
+	explicit Board(const std::vector<Value>& values) 
+		: values_{values} {
 	}
 
 	bool isMarked(int x, int y) const {
@@ -121,6 +134,6 @@ int main() {
 
 	Board winner = calculateWinner(boards, Draw);
 
-	std::cout << "Answer: " << winner.sumUnmarkedValues() * winner.lastDrawNbr << "\n";
+	fmt::print("Answer: {}\n", winner.sumUnmarkedValues() * winner.lastDrawNbr);
 	return 0;
 }
