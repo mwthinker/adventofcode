0b80b0437d5a296d83f2c3999546bd637069dfe3
Marcus Welander <mwthinker@yahoo.se>
2022-10-17 20:08:25 +0200

    Add cli to read custom file to solve problem 2021 06 problem

--- 2021/04/src/main.cpp
+++ 2021/04/src/main.cpp
@@ -1,9 +1,12 @@
 #include <string>
 #include <sstream>
+#include <fstream>
+#include <ostream>
 #include <vector>
 
-
 #include <fmt/core.h>
+#include <fmt/ostream.h>
+#include <lyra/lyra.hpp>
 
 const std::vector Draw{7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1};
 
@@ -27,27 +30,25 @@ const std::string Data = R"(
 	 2  0 12  3  7
 )";
 
+constexpr int BoardSize = 5 * 5;
+
 struct Value {
 	int nbr;
 	bool marked;
 };
 
+//	Create a board from numbers.
+//
+//	Assumes the following format of numbers:
+//		22 13 17 11  0
+//		8   2 23  4 24
+//		21  9 14 16  7
+//		6  10  3 18  5
+//		1  12 20 15 19
 class Board {
 public:
-	// Create empty board.
-	Board() {
-	}
-	
-	//	Create a board from numbers.
-	//
-	//	Assumes the following formats:
-	//		22 13 17 11  0
-	//		8   2 23  4 24
-	//		21  9 14 16  7
-	//		6  10  3 18  5
-	//		1  12 20 15 19
-	explicit Board(const std::vector<Value>& values) 
-		: values_{values} {
+	void addNumber(int nbr) {
+		values_.push_back(Value{nbr, false});
 	}
 
 	bool isMarked(int x, int y) const {
@@ -87,6 +88,13 @@ public:
 		return false;
 	}
 
+
+
+	int calculateWinningNumber() const {
+		return sumUnmarkedValues() * lastDrawNbr;
+	}
+
+private:
 	int sumUnmarkedValues() const {
 		int sum = 0;
 		for (auto& value : values_) {
@@ -114,26 +122,106 @@ Board calculateWinner(std::vector<Board> boards, std::vector<int> numbers) {
 	return {};
 }
 
-int main() {
-	std::stringstream stream(Data);
+struct FileContent {
+	std::vector<int> firstRow;
+	std::vector<int> data;
+};
 
-	std::vector<Board> boards{1};
+std::vector<int> extractNumbers(const std::string& row) {
+	std::vector<int> numbers;
 
-	int value = 0;
-	int index = 0;
-	while (stream >> value) {
-		if (index == boards.size()) {
-			boards.emplace_back();
-		}
+	std::stringstream stream{row};
+	int nbr;
+	while (stream >> nbr) {
+		numbers.push_back(nbr);
+	}
+	return numbers;
+}
 
-		boards[index].values_.push_back({value, false});
-		if (boards[index].values_.size() == 25) {
-			++index;
+FileContent readFromFile(const std::string& filename) {
+	FileContent fileContent;
+
+	std::ifstream infile{filename};
+
+	std::string data;
+	std::getline(infile, data);
+
+	fileContent.firstRow = extractNumbers(data);
+	
+	int nbr;
+	while (infile >> nbr) {
+		fileContent.data.push_back(nbr);
+	}
+
+	return fileContent;
+}
+
+FileContent readDefaultBoard() {
+	FileContent fileContent;
+	fileContent.firstRow = Draw;
+	fileContent.data = extractNumbers(Data);
+
+	return fileContent;
+}
+
+std::vector<Board> extractBoards(const std::vector<int>& numbers) {
+	if (numbers.size() % BoardSize != 0) {
+		throw std::runtime_error{""};
+	}
+
+	int size = static_cast<int>(numbers.size()) / BoardSize;
+	std::vector<Board> boards(size);
+
+	for (int i = 0; i < numbers.size(); ++i) {
+		boards[i / BoardSize].addNumber(numbers[i]);
+	}
+	return boards;
+}
+
+
+template <> struct fmt::formatter<lyra::cli> : ostream_formatter {};
+
+int main(int argc, char** argv) {
+	std::string datafile;
+	bool showHelp = false;
+
+	auto cli
+		= lyra::opt(datafile, "filename")
+		["-d"]["--datafile"]
+		("filename for the problem data") |
+		lyra::help(showHelp);
+
+	auto result = cli.parse(lyra::args(argc, argv));
+	if (!result) {
+		fmt::print(std::cerr, "Error in command line: {}\n", result.message());
+		return 1;
+	}
+
+	if (showHelp) {
+		fmt::print("{}\n", cli);
+		return 0;
+	}
+	
+	std::vector<int> draw;
+	std::vector<int> data;
+	if (datafile.empty()) {
+		draw = Draw;
+		data = extractNumbers(Data);
+	} else {
+		try {
+			auto fileContent = readFromFile(datafile);
+			draw = fileContent.firstRow;
+			data = fileContent.data;
+		} catch (const std::exception& exception) {
+			fmt::print(std::cerr, "Something wrong with the data, error: {}", exception.what());
+			return 1;
 		}
 	}
 
-	Board winner = calculateWinner(boards, Draw);
+	std::vector<Board> boards = extractBoards(data);
 
-	fmt::print("Answer: {}\n", winner.sumUnmarkedValues() * winner.lastDrawNbr);
+	Board winner = calculateWinner(boards, draw);
+
+	fmt::print("Answer: {}\n", winner.calculateWinningNumber());
 	return 0;
 }
